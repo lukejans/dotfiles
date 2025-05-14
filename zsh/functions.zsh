c4f9e49abbb555e3c05682417d168951dff5a2a8
Luke Janssen <91230392+lukejans@users.noreply.github.com>
2025-05-14 18:59:12 -0400

    Fix yazi wrapper function

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -46,8 +46,8 @@ fzo() {
 
 # yazi
 y() {
-  local tmp
-  tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+  local tmp cwd
+  tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
   yazi "${@}" --cwd-file="${tmp}"
   if cwd="$(command cat -- "${tmp}")" && [ -n "${cwd}" ] && [ "${cwd}" != "${PWD}" ]; then
     builtin cd -- "${cwd}" || exit
